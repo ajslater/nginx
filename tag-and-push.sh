488c86a8a01ad342f8805561ae6ea000d3b789bd
AJ Slater <aj@slater.net>
2020-02-26 13:11:00 -0800

    cleanup old scripts and shelllcheck

--- tag-and-push.sh
+++ tag-and-push.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ./env
 VERSION=${ALPINE_VERSION}_${PKG_VERSION}
-TAG=$VERSION-$(echo $CIRCLE_SHA1 | head -c 7);
-docker tag $IMAGE $IMAGE:$TAG;
+TAG=$VERSION-$(echo "$CIRCLE_SHA1" | head -c 7);
+docker tag $IMAGE "$IMAGE:$TAG;"
 docker tag $IMAGE $IMAGE:latest;
 docker push ajslater/nginx
