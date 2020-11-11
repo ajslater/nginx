6a01bc1a4c33acaf8f4fb2ffe81beee5dc973764
AJ Slater <aj@slater.net>
2020-11-10 18:28:13 -0800

    remove circle tag and echo

--- build.sh
+++ build.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 source ./env
-TAG=$VERSION-$(echo "$CIRCLE_SHA1" | head -c 7)
+#TAG=$VERSION-$(echo "$CIRCLE_SHA1" | head -c 7)
 
 function build-platform {
   PLATFORM=$1
-  echo docker build \
+  docker build \
       --platform "$PLATFORM" \
       --file "Dockerfile.${PLATFORM}" \
       --build-arg ALPINE_VERSION=$ALPINE_VERSION \
       --build-arg PKG_VERSION=$PKG_VERSION \
-      --tag "$IMAGE:${PLATFORM}-${TAG}" \
+      --tag "$IMAGE:${PLATFORM}-${VERSION}" \
       --tag "$IMAGE:${PLATFORM}-latest" \
       .
 }
