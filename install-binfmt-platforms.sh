436ab44c9becf64eb44e82b49301cda932c535cf
AJ Slater <aj@slater.net>
2020-11-16 16:32:09 -0800

    minor tweaks

--- install-binfmt-platforms.sh
+++ install-binfmt-platforms.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 set -euxo pipefail
-source ./.env
+source .env
+export DOCKER_CLI_EXPERIMENTAL=enabled
+export DOCKER_BUILDKIT=1
 docker run --rm --privileged tonistiigi/binfmt:latest --install "$PLATFORMS"
